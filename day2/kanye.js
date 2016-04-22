2da0a14554419d4af84c2de8128b9f98c60d1e29
Jarek Wojciechowski <jarekwojo@gmail.com>
2016-04-22 00:07:12 -0400

    Fixed button, and some layout issues

--- day2/kanye.js
+++ day2/kanye.js
@@ -13,7 +13,7 @@ var lines = ["The plan was, to drink until the pain over. But what's worse, the 
 "I just can't seem to get that nigga jokes, but hashtinuminesaymeabi, abi. Andifthatdongetinismin at me, at me.",
 "I never understood planned parenthood, cause I never met nobody planned to be a parent in the hood.",
 "You got the power to let power go.",
-"Too many Urkels on your team, thatâ€™s why your wins low.",
+"Too many Urkels on your team, that's why your wins low.",
 "So much head, I woke up in Sleepy Hollow.",
 "Screams from the haters, got a nice ring to it--I guess every superhero need his theme music.",
 "No one man should have all that power.",
@@ -22,6 +22,10 @@ var lines = ["The plan was, to drink until the pain over. But what's worse, the 
 "When I park my Range Rover, slightly scratch your Corolla--OK, I smash your Corolla."];
 
 var kanyeQuote = function(){
+	console.log('Function Entered');
 	var line = lines[Math.floor(Math.random()*lines.length)];
-	$('#kanyeButton').text(line)
+	console.log(line);
+	$("#kanyeQuote").text(line);
 };
+
+kanyeQuote();
